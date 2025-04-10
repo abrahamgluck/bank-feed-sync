69ab20926d743e035eb2e66d091ecfd6da65232c
Abraham Gluck <36283723+abrahamgluck@users.noreply.github.com>
2025-04-10 17:48:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const axios = require('axios');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+app.get('/test-token', async (req, res) => {
+  try {
+    const response = await axios.post('https://api.finicity.com/aggregation/v2/partners/authentication', null, {
+      headers: {
+        'Finicity-App-Key': process.env.FINICITY_APP_KEY,
+        'Content-Type': 'application/json',
+      },
+      auth: {
+        username: process.env.FINICITY_PARTNER_ID,
+        password: process.env.FINICITY_PARTNER_SECRET,
+      }
+    });
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+    console.log('✅ Access Token:', response.data.token);
+    res.json(response.data);
+  } catch (error) {
+    console.error('❌ Token Error:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Failed to get token' });
+  }
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
